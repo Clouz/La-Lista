8c314f13437caba2e7fe9970bfb6e791735c3db7
Clouz <clouz85@gmail.com>
2018-02-25 18:22:41 +0100

    Search API themoviedb finito

--- main.go
+++ main.go
@@ -1,98 +1,40 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"net/http"
-	"net/url"
-	"strconv"
+	"log"
+	"path"
+	"regexp"
+	"strings"
 )
-
-//API info: https://developers.themoviedb.org/3/search/search-movies
-var site = "https://api.themoviedb.org/3/search/movie?"
-var apiKey = "api_key=da6f7ad5c8626cea07130cdf023dab07"
-
-type searchMovie struct {
-	language           string //Pass a ISO 639-1 value to display translated data
-	query              string //Pass a text query to search
-	page               string //Specify which page to query
-	includeAdult       bool   //Choose whether to inlcude adult content in the results
-	region             string //Specify a ISO 3166-1 code to filter release dates
-	year               int
-	primaryReleaseYear int
-}
-
-type schema struct {
-	page          int
-	results       []result
-	total_results int
-	total_pages   int
-}
-
-type result struct {
-	poster_path       string
-	adult             bool
-	overview          string
-	release_date      string
-	genre_ids         string
-	id                int
-	original_title    string
-	original_language string
-	title             string
-	backdrop_path     string
-	popularity        float32
-	vote_count        int
-	video             bool
-	vote_average      float32
-}
-
-func (m searchMovie) compose() string {
-	result := site + apiKey
-	if m.language != "" {
-		result = result + "&language=" + m.language
-	}
-	if m.query != "" {
-		result = result + "&query=" + url.QueryEscape(m.query)
-	}
-	if m.page != "" {
-		result = result + "&page=" + m.page
-	}
-	if m.includeAdult != false {
-		result = result + "&include_adult=" + "true"
-	}
-	if m.region != "" {
-		result = result + "&region=" + m.region
-	}
-	if m.year != 0 {
-		result = result + "&year=" + strconv.Itoa(m.year)
-	}
-	if m.primaryReleaseYear != 0 {
-		result = result + "&primary_release_year=" + strconv.Itoa(m.primaryReleaseYear)
-	}
-
-	return result
-}
 
 func main() {
 
-	ciao := searchMovie{year: 2018, query: "Black Panther"}
-	url := ciao.compose()
-	fmt.Println(url)
+	// ciao := themoviedb.SearchMovie{Year: 2018, Query: "Black Panther", Language: "it-IT"}
+	// x, _ := ciao.Search()
 
-	resp, err := http.Get(url)
+	// fmt.Print(x.Results[0].Overview)
+
+	files, err := ioutil.ReadDir(".")
 	if err != nil {
-		fmt.Println("NewRequest: ", err)
-		return
+		log.Fatal(err)
 	}
 
-	str, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(str))
+	for _, file := range files {
+		if !file.IsDir() {
 
-	var x schema
+			file := file.Name()
+			ext := path.Ext(file)
+			movie := strings.TrimSuffix(file, ext)
 
-	json.NewDecoder(resp.Body).Decode(&x)
+			r, _ := regexp.Compile(` \([0-9]{4}\)`)
+			i := r.FindStringIndex(movie)
 
-	fmt.Print(x)
-
+			if i != nil {
+				//TODO: da dividere l'anno dal nome film
+				fmt.Println(movie)
+			}
+		}
+	}
 }
---
+++ themoviedb/search.go
@@ -0,0 +1,97 @@
+package themoviedb
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+//API info: https://developers.themoviedb.org/3/search/search-movies
+var site = "https://api.themoviedb.org/3/search/movie?"
+var apiKey = "api_key=da6f7ad5c8626cea07130cdf023dab07"
+
+// SearchMovie Search for movies.
+type SearchMovie struct {
+	Language           string //Pass a ISO 639-1 value to display translated data
+	Query              string //Pass a text query to search
+	Page               string //Specify which page to query
+	IncludeAdult       bool   //Choose whether to inlcude adult content in the results
+	Region             string //Specify a ISO 3166-1 code to filter release dates
+	Year               int
+	PrimaryReleaseYear int
+}
+
+// Schema Responses
+type Schema struct {
+	Page         int      `json:"page"`
+	Results      []Result `json:"results"`
+	TotalResults int      `json:"total_results"`
+	TotalPages   int      `json:"total_pages"`
+}
+
+// Result Movie List Result Object
+type Result struct {
+	PosterPath       string  `json:"poster_path"`
+	Adult            bool    `json:"adult"`
+	Overview         string  `json:"overview"`
+	ReleaseDate      string  `json:"release_date"`
+	GenreIds         string  `json:"genre_ids"`
+	ID               int     `json:"Id"`
+	OriginalTitle    string  `json:"original_title"`
+	OriginalLanguage string  `json:"original_language"`
+	Title            string  `json:"title"`
+	BackdropPath     string  `json:"backdrop_path"`
+	Popularity       float32 `json:"popularity"`
+	VoteCount        int     `json:"vote_count"`
+	Video            bool    `json:"video"`
+	VoteAverage      float32 `json:"vote_average"`
+}
+
+// Compose return the URL for the quey
+func (m SearchMovie) Compose() string {
+	result := site + apiKey
+	if m.Language != "" {
+		result = result + "&language=" + m.Language
+	}
+	if m.Query != "" {
+		result = result + "&query=" + url.QueryEscape(m.Query)
+	}
+	if m.Page != "" {
+		result = result + "&page=" + m.Page
+	}
+	if m.IncludeAdult != false {
+		result = result + "&include_adult=" + "true"
+	}
+	if m.Region != "" {
+		result = result + "&region=" + m.Region
+	}
+	if m.Year != 0 {
+		result = result + "&year=" + strconv.Itoa(m.Year)
+	}
+	if m.PrimaryReleaseYear != 0 {
+		result = result + "&primary_release_year=" + strconv.Itoa(m.PrimaryReleaseYear)
+	}
+
+	return result
+}
+
+// Search do a movie search query to TheMovieDb
+func (m SearchMovie) Search() (Schema, error) {
+
+	var schema Schema
+
+	url := m.Compose()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return schema, err
+	}
+
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&schema)
+	return schema, nil
+}
