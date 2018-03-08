0c2e1daadd5cc60e4fb3d6da6b4ca0a2e38f190a
Clouz <clouz85@gmail.com>
2018-03-09 00:00:06 +0100

    ricavato anno e nome file

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"La-Lista/themoviedb"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"path"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -27,14 +29,20 @@ func main() {
 			file := file.Name()
 			ext := path.Ext(file)
 			movie := strings.TrimSuffix(file, ext)
+			year := ""
 
-			r, _ := regexp.Compile(` \([0-9]{4}\)`)
+			r, _ := regexp.Compile(` \(([0-9]{4})\)`)
 			i := r.FindStringIndex(movie)
 
 			if i != nil {
-				//TODO: da dividere l'anno dal nome film
-				fmt.Println(movie)
+				year = r.FindStringSubmatch(movie)[1]
+				movie = strings.Replace(movie, r.FindString(movie), "", -1)
 			}
+
+			y, _ := strconv.Atoi(year)
+			tmdb := themoviedb.SearchMovie{Query: movie, Year: y}
+			x, _ := tmdb.Search()
+			fmt.Println(x.Results[0].Overview)
 		}
 	}
 }
