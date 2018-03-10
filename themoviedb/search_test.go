51589c31d93cc28c12a46ab1a82c711c2c6ca833
Clouz <clouz85@gmail.com>
2018-03-10 19:08:45 +0100

    Add unit test for Themoviedb Search

---
+++ themoviedb/search_test.go
@@ -0,0 +1,15 @@
+package themoviedb_test
+
+import (
+	"La-Lista/themoviedb"
+	"testing"
+)
+
+func TestSearch(t *testing.T) {
+	x := themoviedb.SearchMovie{Query: "Deadpool", Year: 2016, IncludeAdult: true, Language: "IT-it"}
+	s, err := x.Search()
+	if err != nil {
+		t.Errorf("Error in the Search %d", err)
+	}
+	t.Logf("%s", s.Results[0].Title)
+}
