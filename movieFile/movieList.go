7ad94020de037b823a2487aed2ded3851210108b
Clouz <clouz85@gmail.com>
2018-03-11 13:56:30 +0100

    Add TODO

--- movieFile/movieList.go
+++ movieFile/movieList.go
@@ -14,6 +14,7 @@ import (
 // validExt represent hello
 var validExt = map[string]bool{
 	".testMovie": true,
+	// TODO: Add all valid file extension
 }
 
 // Movie contain all the info given in the filename
