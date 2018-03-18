66ea724ca60d8a7537ad815f9be4769150d22d59
Clouz <clouz85@gmail.com>
2018-03-18 18:15:02 +0100

    General regexp for movie filename

--- movieFile/rawFilename.go
+++ movieFile/rawFilename.go
@@ -10,14 +10,22 @@ func RawFilename() string {
 
 	//f2 := "La.Forma.dell.acqua.2017.720p.ita.dts.eng.aac.subs.fd.mkw"
 	content := "Justice.League.2017.DVD.Rip.AVC.x264.AC3.ITA.AAC.ENG.Subs.^MF^.mkv"
-	regexp := regexp.MustCompile(`^(?P<name>.+)\.(?P<year>\d{4}).+\.(?P<ext>mp4|avi|mkv)$`)
-	template := "$name ($year).$ext"
+
+	// Regex pattern captures "key: value" pair from the content.
+	pattern := regexp.MustCompile(`^(?P<name>.+)\.(?P<year>\d{4}).+\.(?P<ext>mp4|avi|mkv)$`)
+
+	// Template to convert "key: value" to "key=value" by
+	// referencing the values captured by the regex pattern.
+	template := "$name ($year).$ext\n"
+
 	result := []byte{}
 
-	for _, submatches := range regexp.FindAllStringSubmatchIndex(content, -1) {
-		regexp.ExpandString(result, template, content, submatches)
+	// For each match of the regex in the content.
+	for _, submatches := range pattern.FindAllStringSubmatchIndex(content, -1) {
+		// Apply the captured submatches to the template and append the output
+		// to the result.
+		result = pattern.ExpandString(result, template, content, submatches)
 	}
-
 	fmt.Println(string(result))
 
 	return newName
